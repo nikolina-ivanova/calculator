05adb210fb2ce7b4e034648d3b2ce398e28a8cc9
Nikolina Ivanova <nikolina9805@abv.bg>
2021-10-26 13:41:14 +0300

    js changed

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-function calc(x) {
-    const result = document.getElementById("result").value+=x;
-}
-function clearCalc() {
-    result.value = "";
-}
-function runResult() {
-    result.value = eval(result.value)
-}
-
 //show current time
 function updateTime() {
-
-
     const currentTimeToday = new Date();
     const currentTime = `${currentTimeToday.getHours()} : ${currentTimeToday.getMinutes()} : ${currentTimeToday.getSeconds()}`
     const showCurrentTime = document.getElementById("currentTime")
     showCurrentTime.innerText = currentTime
 }
 setInterval(updateTime, 1000);
+
+// calculator
+const inputText = document.getElementById('result')
+const button = document.querySelectorAll('input[type=button]')
+
+const getResult = () => {
+    if(inputText.value != '')
+        inputText.value = eval(inputText.value);
+};
+const emptyInputText = () => {
+    inputText.value = '';
+};
+
+
+
+
+button.forEach((el) => {
+        el.addEventListener('click', (e) => {
+            if(el.value == '='){
+                getResult();  
+            }
+            else if(el.value == 'C'){
+                emptyInputText();
+            }
+            else{
+                inputText.value += el.value
+            }
+        })
+})
+
